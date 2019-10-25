2af24f741ce1efcd72614d5577488aa01552a01a
janzo <giovanniberi93@gmail.com>
2019-10-25 20:41:13 +0200

    wip

---
+++ leetcode/distantBarcodes/exercise.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <map>
+
+class Solution {
+public:
+    std::vector<int> rearrangeBarcodes(std::vector<int>& barcodes) 
+    {
+        std::vector<int> result;
+        std::vector<std::pair<int, int>> occurrences;
+        std::map<int, int> nrBarcodes;
+
+        for (auto code : barcodes)
+        {
+            auto targetCodePosition = nrBarcodes.find(code);
+            if (targetCodePosition == nrBarcodes.end())
+                nrBarcodes.insert(std::pair<int, int>(code, 1));
+            else
+                nrBarcodes[code]++;
+        }
+
+        for (auto it = nrBarcodes.begin(); it != nrBarcodes.end(); ++it)
+        {
+            occurrences.push_back(std::pair<int, int>(it->first, it->second));
+        }
+        std::sort(occurrences.begin(), occurrences.end(), [](std::pair<int, int> el1, std::pair<int, int> el2){
+                return el1.second > el2.second;});
+        
+        for (int currentMainValueIndex = 0; currentMainValueIndex < occurrences.size(); ++currentMainValueIndex)
+        {
+            int mainValue = occurrences[currentMainValueIndex].first;
+            int mainValueOccurences = occurrences[currentMainValueIndex].second;
+
+            for(int valuesInserted = 0; valuesInserted < mainValueOccurences; ++valuesInserted)
+            {
+                result.push_back(mainValue);
+                if (result.size() == barcodes.size())
+                    return result;
+
+                int valueToInterponeIndex;
+                for (valueToInterponeIndex = currentMainValueIndex + 1;
+                     valueToInterponeIndex < occurrences.size();
+                     valueToInterponeIndex++)    
+                {
+                    if (valueToInterponeIndex == occurrences.size() - 1)
+                        break;
+                    if (occurrences[valueToInterponeIndex + 1].second < occurrences[valueToInterponeIndex].second) 
+                        break;
+                }
+                result.push_back(occurrences[valueToInterponeIndex].first);
+                occurrences[valueToInterponeIndex].second--;
+            }
+        }
+        return result;
+    }
+};
+
+template <typename T>
+void printVector(std::vector<T> v)
+{
+    for (auto el : v)
+        std::cout << el << " ";
+    std::cout << std::endl;
+}
+
+int main(int argc, char **argv) 
+{ 
+    Solution s;
+
+    // std::vector<int> input = {1,1,1,2,2,2};
+    std::vector<int> input = {7,7,7,8,5,7,5,5,5,8};
+    std::vector<int> result = s.rearrangeBarcodes(input);
+
+    printVector(result);
+}
